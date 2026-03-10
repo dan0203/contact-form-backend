791b77c2b5ac8ee0516d3a09abde2b4c4d66533e
Dan Zerbib <danzerbib@gmail.com>
2026-03-10 23:55:17 +0100

    test console.log url reçue par northflank

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    console.log('METHOD:', req.method);
+    console.log('URL:', req.originalUrl);
+    console.log('PATH:', req.path);
+    next();
+});
 
 app.post('/contact-form', (req, res) => {
     try {
